0ff1b06a0584329e9cb43941a7c42f2b457b4444
treeform <starplant@gmail.com>
2019-10-17 21:16:01 +0000

    add import all file

---
+++ src/jsutils.nim
@@ -0,0 +1,14 @@
+import jsutils/strings
+export jsutils/strings
+
+import jsutils/canvas
+export jsutils/canvas
+
+import jsutils/arraybuffers
+export jsutils/arraybuffers
+
+import jsutils/jsons
+export jsutils/jsons
+
+import jsutils/streams
+export jsutils/streams
