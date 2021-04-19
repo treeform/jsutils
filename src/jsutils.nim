f5b19ff6028a8dea0194e4a0be42ed7710f28c31
Andre von Houck <starplant@gmail.com>
2021-04-19 19:00:48 +0000

    Add js-based websockets.

--- src/jsutils.nim
+++ src/jsutils.nim
@@ -1,3 +1,3 @@
 import jsutils/strings, jsutils/canvas, jsutils/arraybuffers, jsutils/jsons,
-  jsutils/streams, jsutils/doms
-export strings, canvas, arraybuffers, jsons, streams, doms
+  jsutils/streams, jsutils/doms, jsutils/websockets
+export strings, canvas, arraybuffers, jsons, streams, doms, websockets
---
+++ src/jsutils/websockets.nim
@@ -0,0 +1,34 @@
+from dom import Event, EventTarget
+
+# based on https://github.com/stisa/jswebsockets by Silvio MIT Copyright 2017
+
+type
+  MessageEvent* = object of Event
+    data*: cstring
+    origin*: cstring
+
+  CloseEvent*  = object of Event
+    code*: Natural
+    reason*: cstring
+    wasClean*: bool
+
+  ReadyState* = enum
+    Connecting = 0
+    Open = 1
+    Closing = 2
+    Closed = 3
+
+  WebSocket* = ref object of EventTarget
+    url*: cstring
+    protocol*: cstring
+    readyState*: ReadyState
+    onopen*: proc (event: Event)
+    onclose*: proc (event: CloseEvent)
+    onmessage*: proc (event: MessageEvent)
+
+proc newWebSocket*(url: cstring): WebSocket {.importcpp: "new WebSocket(#)".}
+proc newWebSocket*(url: cstring, protocol: cstring): WebSocket {.importcpp: "new WebSocket(#,@)".}
+proc newWebSocket*(url: cstring, protocols: seq[cstring]): WebSocket {.importcpp: "new WebSocket(#,@)".}
+proc addEventListener*(et: EventTarget, ev: cstring, cb: proc (ev: MessageEvent | CloseEvent)) {.importcpp.}
+proc send*(socket: WebSocket, data: cstring) {.importcpp.}
+proc close*(socket: WebSocket) {.importcpp.}
